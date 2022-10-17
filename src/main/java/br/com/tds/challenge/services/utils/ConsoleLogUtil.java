7314dfcecf4dca1cbee06d4644dca47a0d0a9bdc
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-16 23:59:17 -0300

    CHORE [Created Class For Facilitate Console Log Messages]

--- src/main/java/br/com/tds/challenge/services/utils/ConsoleLogUtil.java
+++ src/main/java/br/com/tds/challenge/services/utils/ConsoleLogUtil.java
@@ -1,2 +1,13 @@
-package br.com.tds.challenge.services.utils;public class ConsoleLogUtil {
+package br.com.tds.challenge.services.utils;
+
+import br.com.tds.challenge.services.schedules.UrlDailyAccessSchedule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsoleLogUtil {
+    //region VARIABLES
+    public static final Logger log = LoggerFactory.getLogger(UrlDailyAccessSchedule.class);
+    //endregion
 }
