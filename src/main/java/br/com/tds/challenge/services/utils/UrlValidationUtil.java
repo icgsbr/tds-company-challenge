79e33066a96bfa2f5ae75934f196730610baf392
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-16 23:53:55 -0300

    FEAT [Url Validation Utility Class Creation]: This class will check wether the url provided by the user is valid or not

--- src/main/java/br/com/tds/challenge/services/utils/UrlValidationUtil.java
+++ src/main/java/br/com/tds/challenge/services/utils/UrlValidationUtil.java
@@ -1,2 +1,19 @@
-package br.com.tds.challenge.services.utils;public class UrlValidationUtil {
+package br.com.tds.challenge.services.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+
+@Component
+public class UrlValidationUtil {
+    public static boolean isUrlValid(String url) {
+        try {
+            new URL(url).toURI();
+            ConsoleLogUtil.log.info("URL VALIDATION: VALID URL");
+            return true;
+        } catch (Exception e) {
+            ConsoleLogUtil.log.info("URL VALIDATION: INVALID URL");
+            return false;
+        }
+    }
 }
