7f05b66bee05bbc5c8374a9f2088d345a47e7fba
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-16 23:40:07 -0300

    FEAT [Created DTO]

--- src/main/java/br/com/tds/challenge/dtos/UrlDTO.java
+++ src/main/java/br/com/tds/challenge/dtos/UrlDTO.java
@@ -1,2 +1,17 @@
-package br.com.tds.challenge.dtos;public class UrlDTO {
+package br.com.tds.challenge.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlDTO {
+    //region ATTRIBUTES
+    @NotBlank
+    String longUrl;
+    //endregion
 }
