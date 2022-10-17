e95bcee544b5bc7d4c35b2a8904d691c047c3396
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-16 23:39:19 -0300

    FEAT [Created Url Model/Entity]

--- src/main/java/br/com/tds/challenge/models/Url.java
+++ src/main/java/br/com/tds/challenge/models/Url.java
@@ -1,2 +1,42 @@
-package br.com.tds.challenge.models;public class Url {
+package br.com.tds.challenge.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "url")
+public class Url {
+    //region ATTRIBUTES
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String longUrl;
+
+    private String shortUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+    private Integer numberOfAccesses = 0;
+
+    private Double dailyAccessAverage = 0.0;
+
+    private LocalDateTime lastAccessDate;
+    //endregion
+
+    //region CONSTRUCTORS
+    public Url(String longUrl) {
+        this.creationDate = LocalDateTime.now();
+        this.longUrl = longUrl;
+    }
+    //endregion
 }
