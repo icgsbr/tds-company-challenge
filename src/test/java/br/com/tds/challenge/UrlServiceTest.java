ee2dd4ec8d74a3ba69362378271715f584cac675
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-17 14:40:38 -0300

    TEST [Implemented Url Service Tests]

--- src/test/java/br/com/tds/challenge/UrlServiceTest.java
+++ src/test/java/br/com/tds/challenge/UrlServiceTest.java
@@ -11,8 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.time.LocalDate;
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -26,47 +24,41 @@ public class UrlServiceTest {
     UrlService urlService;
 
     @Test
-    public void encode_lessThan62() {
-        assertEquals("k", UrlCodificationUtil.encode(10));
+    public void convertToShortUrlTest() {
+        Url url = new Url("https://github.com/tdscompany/desafio-backend-encurtador-url");
+        url.setId(5L);
+
+        when(mockUrlRepository.save(any(Url.class))).thenReturn(url);
+        assertEquals("f", UrlCodificationUtil.encode(url.getId()));
+
+        UrlDTO urlDTO = new UrlDTO("https://github.com/tdscompany/desafio-backend-encurtador-url");
+
+        assertEquals("http://localhost:8080/f", urlService.convertToShortUrl(urlDTO));
     }
 
     @Test
-    public void encode_moreThan62() {
-        assertEquals("bq", UrlCodificationUtil.encode(78));
+    public void getOriginalUrlTest() {
+        assertEquals(7L, UrlCodificationUtil.decode("h"));
+
+        Url url = new Url("https://github.com/tdscompany/desafio-backend-encurtador-url");
+        url.setId(7L);
+
+        when(mockUrlRepository.findById(7L)).thenReturn(java.util.Optional.of(url));
+        assertEquals("https://github.com/tdscompany/desafio-backend-encurtador-url", urlService.getOriginalUrl("h"));
     }
 
     @Test
-    public void decode_singleCharacter() {
-        assertEquals(11, UrlCodificationUtil.decode("l"));
+    public void dailyAccessCalculation() {
+        Url url = new Url("https://www.youtube.com/");
+        url.setId(1L);
+
+        Double passedDays = 2.0;
+        Integer numberOfAccesses = 10;
+
+        double result = (numberOfAccesses / passedDays);
+
+        assertEquals("5.0", Double.toString(result));
+
+        urlService.calculateDailyAccessAverage(url.getId(), passedDays, numberOfAccesses);
     }
-//    TODO: Adjust Tests
-//    @Test
-//    public void convertToShortUrlTest() {
-//        Url url = new Url();
-//        url.setLongUrl("https://github.com/AnteMarin/UrlShortener-API");
-//        url.setCreationDate(LocalDate.now());
-//        url.setId(16L);
-//
-//        when(mockUrlRepository.save(any(Url.class))).thenReturn(url);
-//        when(urlService.encode(url.getId())).thenReturn("f");
-//
-//        UrlDTO urlRequest = new UrlDTO();
-//        urlRequest.setLongUrl("https://github.com/AnteMarin/UrlShortener-API");
-//
-//        assertEquals("f", urlService.convertToShortUrl(urlRequest));
-//    }
-//
-//    @Test
-//    public void getOriginalUrlTest() {
-//        when(urlService.decode("h")).thenReturn((long) 7);
-//
-//        Url url = new Url();
-//        url.setLongUrl("https://github.com/AnteMarin/UrlShortener-API");
-//        url.setCreationDate(LocalDate.now());
-//        url.setId(7L);
-//
-//        when(mockUrlRepository.findById((long) 7)).thenReturn(java.util.Optional.of(url));
-//        assertEquals("https://github.com/AnteMarin/UrlShortener-API", urlService.getOriginalUrl("h"));
-//
-//    }
 }
