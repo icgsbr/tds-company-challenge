0d79071f66d617263328d9a3a6f21fddfe65cdb0
Igor Costa <ext.igor.costa@o-pitblast.com>
2022-10-16 23:51:25 -0300

    FEAT [Implemented Url Codification Logic]: Create utility class that will be responsible for url codification

--- src/main/java/br/com/tds/challenge/services/utils/UrlCodificationUtil.java
+++ src/main/java/br/com/tds/challenge/services/utils/UrlCodificationUtil.java
@@ -1,2 +1,44 @@
-package br.com.tds.challenge.services.utils;public class UrlCodificationUtil {
+package br.com.tds.challenge.services.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UrlCodificationUtil {
+    //region VARIABLES
+    private static final String allowedString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final char[] allowedCharacters = allowedString.toCharArray();
+    private static final int base = allowedCharacters.length;
+    //endregion
+
+    //region METHODS
+    public static String encode(long urlId) {
+        StringBuilder encodedString = new StringBuilder();
+
+        if(urlId == 0) {
+            return String.valueOf(allowedCharacters[0]);
+        }
+
+        while (urlId > 0) {
+            encodedString.append(allowedCharacters[(int) (urlId % base)]);
+            urlId = urlId / base;
+        }
+
+        return encodedString.reverse().toString();
+    }
+
+    public static long decode(String shortUrlComplement) {
+        char[] characters = shortUrlComplement.toCharArray();
+
+        long decoded = 0;
+
+        //counter is used to avoid reversing input string
+        int counter = 1;
+        for (char character : characters) {
+            decoded += allowedString.indexOf(character) * Math.pow(base, characters.length - counter);
+            counter++;
+        }
+
+        return decoded;
+    }
+    //endregion
 }
